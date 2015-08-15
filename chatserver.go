ef6a4f2ccbad55052efe65fcef48f169ddd9e920
LyudvaDmitry <lyudva.dmitry@gmail.com>
2015-08-15 13:02:23 +0300

    Reworked architecture. Fixed some issues.

--- chatserver.go
+++ chatserver.go
@@ -2,7 +2,6 @@ package chatserver
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -21,188 +20,116 @@ const (
 		"Currently there are %v user(s) is the chat%v.\r\n"
 )
 
-var (
-	users Users
-	//k = 0
-	//count = make(map[string]int)
-)
-
-type Users struct {
-	list map[string]*user
+type Chatserver struct {
+	userlist map[string]*user
 	sync.Mutex
 }
 
-type user struct {
-	name string
-	conn     net.Conn
-	input    chan message
-}
-
-//Quit closes connection, removes user from list and informs users that
-//given user has left the chat.
-func (User user) quit() {
-	log.Printf("%v (%v) has left the chat", User.name, User.conn.RemoteAddr())
-	User.conn.Close()
-	users.Lock()
-	delete(users.list, User.name)
-	users.Unlock()
-	message{system, all, fmt.Sprintf("User %v has left the chat\r\n", User.name)}.send()
-}
-
-type message struct {
-	from string
-	to   string
-	string
-}
-
-//message.send sends message to the according channels.
-func (mes message) send() {
-	users.Lock()
-	if mes.to == all {
-		for _, u := range users.list {
-			u.input <- mes
-		}
-	} else {
-		users.list[mes.to].input <- mes
-	}
-	//k++
-	//count[mes.from]++
-	users.Unlock()
-	//log.Printf("(%v) %v to %v: %v", k, mes.from, mes.to, strings.TrimSpace(mes.string))
-	log.Printf("%v to %v: %v", mes.from, mes.to, strings.TrimSpace(mes.string))
-}
-
-func main(l net.Listener) {
-	users.list = make(map[string]*user)
+//Run initializes server and starts handling connections.
+func (cs *Chatserver) Run() {
+	cs.userlist = make(map[string]*user)
 	l, err := net.Listen("tcp", ":2000")
 	if err != nil {
 		log.Fatal(err)
 	}
-	//Close l
+	defer l.Close()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Println(err, "1")
+			log.Println(err)
 			continue
 		}
 		log.Printf("%v connected", conn.RemoteAddr())
-
-		users.Lock()
-		fmt.Fprintf(conn, welcomeMessage, len(users.list), users.UserList())
-		users.Unlock()
-		Name, User, err := NewUser(conn)
-		//Trying to get right username till user give up.
-		for err != nil {
-			switch {
-			case err.Error() == "User already exists":
-				fmt.Fprintf(conn, "Error: this username is occuped\r\n")
-			default:
-				log.Println(err, "2")
-				fmt.Fprintf(conn, "Error reading username, try again.\r\n")
-			}
-			Name, User, err = NewUser(conn)
-		}
-
-		users.Lock()
-		users.list[Name] = User
-		go users.list[Name].receive()
-		go users.list[Name].send()
-		users.Unlock()
-		message{system, Name, fmt.Sprintf("Hello, %v. You can start chatting now.\r\n", Name)}.send()
+		fmt.Fprintf(conn, welcomeMessage, cs.Len(), cs.List())
+		cs.addUser(conn)
 	}
 }
 
-//NewUser returns user, its name and error, if any. 
-func NewUser(conn net.Conn) (string, *user, error) {
-	//Can't use send() or receive() here as there is no user for now.
-	fmt.Fprintf(conn, "Please, enter your username\r\n")
-	name, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		return name, nil, err
-	}
-	name = backspaceFighter(strings.TrimSpace(name))
-	users.Lock()
-	_, present := users.list[name];
-	users.Unlock()
-	if present {
-		return name, nil, errors.New("User already exists")
-	}
-	log.Printf("%v joined the chat as %v\n", conn.RemoteAddr(), name)
-	message{system, all, fmt.Sprintf("%v entered chat\r\n", name)}.send()
-	return name, &user{name, conn, make(chan message)}, nil
-}
-
-//Send waits for messages to be sent to the user.input channel
-//and send it to user using user.conn.
-func (User *user) send() {
-	for mes := range User.input {
-		log.Printf("from: %v, to: %v\r\n%v", mes.from, mes.to, mes.string)
-		fmt.Fprintf(User.conn, "from: %v, to: %v\r\n%v", mes.from, mes.to, mes.string)
-	}
-}
-
-//Receive receives messages from user and parses it. Then it send message or
-//execute command depends on the message content.
-func (User *user) receive() {
+//addUser requests new user to enter his username, creates user object,
+//adds it to userlist and call handleUser().
+func (cs *Chatserver) addUser(conn net.Conn) {
+	//Trying to get right username till user give up.
+	var name string
+	var err error
 	for {
-		//Reading and parsing
-		str, err := bufio.NewReader(User.conn).ReadString('\n')
+		fmt.Fprintf(conn, "Please, enter your username\r\n")
+		name, err = cs.getStr(conn)
 		if err != nil {
-			log.Println(err)
-			User.quit()
-			return
-		}
-		str = backspaceFighter(str)
-		log.Print(User.name + ": " + str)
-		//users.Lock()
-		//k--
-		//users.Unlock()
-		//Creating message and executing commands
-		var mes message
-		switch {
-		case str[0] != '\\':
-			mes = message{User.name, all, str}
-		case strings.HasPrefix(str, "\\to:"):
-			str := strings.SplitN(strings.TrimPrefix(str, "\\to:"), " ", 2)
-			users.Lock()
-			_, present := users.list[str[0]]
-			users.Unlock()
-			if !present {
-				message{system, User.name, fmt.Sprintf("Error: no such user as %v\r\n", strings.TrimSpace(str[0]))}.send()
-				continue
-			}
-			mes = message{User.name, str[0], str[1]}
-		case strings.HasPrefix(str, "\\quit"):
-			User.quit()
-			return
-		default:
-			message{system, User.name, fmt.Sprint("Unknown command\r\n")}.send()
+			fmt.Fprintf(conn, "Error reading username, try again.\r\n")
 			continue
 		}
-		log.Print(User.name + ":(almost there) " + str)
-		mes.send()
+		if cs.Get(name) != nil {
+			fmt.Fprintf(conn, "Error: this username is occuped\r\n")
+			continue
+		}
+		break
 	}
+	log.Printf("%v joined the chat as %v\n", conn.RemoteAddr(), name)
+	cs.Send(message{system, all, fmt.Sprintf("%v entered chat\r\n", name)})
+	u := &user{name, conn, make(chan message)}
+	cs.Lock()
+	cs.userlist[name] = u
+	cs.Unlock()
+	cs.handleUser(u)
+	cs.Send(message{system, name, fmt.Sprintf("Hello, %v. You can start chatting now.\r\n", name)})
 }
 
-//Userlist returns list of users currently online.
-func (users *Users) UserList() string {
-	temp := make([]string, 0, len(users.list))
-	for _, user := range users.list {
-		temp = append(temp, user.name)
-	}
-	str := strings.Join(temp, ", ")
-	if cap(temp) > 0 {
-		str = ": " + str
-	}
-	return str
+//handleUser runs goroutines implementing communication with user. They stop as soon
+//as chatserver.Delete is called.
+func (cs *Chatserver) handleUser(User *user) {
+	//Messages for user
+	go func() {
+		for mes := range User.input {
+			fmt.Fprintf(User.conn, "from: %v, to: %v\r\n%v", mes.from, mes.to, mes.string)
+		}
+	}()
+	//Messages from user
+	go func() {
+		for {
+			str, err := cs.getStr(User.conn)
+			if err != nil {
+				log.Println(err)
+				cs.Delete(User.name)
+				return
+			}
+			//Creating message and executing commands
+			var mes message
+			switch {
+			case str[0] != '\\':
+				mes = message{User.name, all, str + "\r\n"}
+			case strings.HasPrefix(str, "\\to:"):
+				comm := strings.SplitN(strings.TrimPrefix(str, "\\to:"), " ", 2)
+				if cs.Get(comm[0]) == nil {
+					cs.Send(message{system, User.name, fmt.Sprintf("Error: no such user as %v\r\n", comm[0])})
+					continue
+				}
+				if len(comm) == 1 {
+					comm = append(comm, "")
+				}
+				mes = message{User.name, comm[0], comm[1] + "\r\n"}
+			case strings.HasPrefix(str, "\\quit"):
+				cs.Delete(User.name)
+				return
+			default:
+				cs.Send(message{system, User.name, fmt.Sprint("Unknown command\r\n")})
+				continue
+			}
+			cs.Send(mes)
+		}
+	}()
 }
 
-//backspaceFighter "executes" backspaces.
-func backspaceFighter (input string) string {
-	in := []byte(input)
-	res := make([]byte, len(input))
+//getStr reads string from given connection and parses it.
+func (cs *Chatserver) getStr(conn net.Conn) (string, error) {
+	str, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	str = strings.TrimSpace(str)
+	runes := []rune(str)
+	res := make([]rune, len(str))
 	i := 0
-	for _, char := range in {
+	for _, char := range runes {
 		switch char {
 		case '\b':
 			i--
@@ -212,5 +139,71 @@ func backspaceFighter (input string) string {
 		}
 	}
 	res = res[:i]
-	return string(res)
+	return string(res), nil
+}
+
+//Delete deletes user from chatserver and closes its connection.
+func (cs *Chatserver) Delete(name string) {
+	cs.Lock()
+	log.Printf("%v (%v) has left the chat", name, cs.userlist[name].conn.RemoteAddr())
+	close(cs.userlist[name].input)
+	cs.userlist[name].conn.Close()
+	delete(cs.userlist, name)
+	cs.Unlock()
+	cs.Send(message{system, all, fmt.Sprintf("User %v has left the chat\r\n", name)})
+}
+
+//Get returns user with given username.
+func (cs *Chatserver) Get(name string) *user {
+	cs.Lock()
+	defer cs.Unlock()
+	return cs.userlist[name]
+}
+
+//List returns list of users currenty online as string.
+func (cs *Chatserver) List() string {
+	temp := make([]string, 0, cs.Len())
+	cs.Lock()
+	for _, user := range  cs.userlist {
+		temp = append(temp, user.name)
+	}
+	cs.Unlock()
+	str := strings.Join(temp, ", ")
+	if cap(temp) > 0 {
+		str = ": " + str
+	}
+	return str
+}
+
+//Len returns current number of users.
+func (cs *Chatserver) Len() int {
+	cs.Lock()
+	defer cs.Unlock()
+	return len(cs.userlist)
+}
+
+//Send sends given message according to its 'from' and 'to' fields.
+func (cs *Chatserver) Send(mes message) {
+	cs.Lock()
+	defer cs.Unlock()
+	log.Printf("Mes(%v->%v): %v", mes.from, mes.to, mes.string)
+	if mes.to == all {
+		for _, u := range cs.userlist {
+			u.input <- mes
+		}
+	} else {
+		cs.userlist[mes.to].input <- mes
+	}
+}
+
+type user struct {
+	name string
+	conn     net.Conn
+	input    chan message
+}
+
+type message struct {
+	from string
+	to   string
+	string
 }
--- chatserver_test.go
+++ chatserver_test.go
@@ -1,42 +1,47 @@
 package chatserver
 
 import (
-//	"bufio"
 	"testing"
 	"fmt"
 	"net"
 	"log"
 	"time"
 	"sync"
-//	"os"
+	"math/rand"
 )
 
-func TestMain(t *testing.T) {
-	var l *net.TCPListener
-	go main(l)
-	defer l.Close()
+const (
+	botnum = 10
+	mesnum = 10
+)
+
+func TestRun(t *testing.T) {
+	var cs Chatserver
+	go cs.Run()
 	var wg sync.WaitGroup
-    wg.Add(10)
-	for i := 0; i < 10; i++ {
+    wg.Add(botnum)
+	for i := 0; i < botnum; i++ {
 		go func(i int) {
 			conn, err := net.Dial("tcp", "127.0.0.1:2000")
 			if err != nil {
 				log.Fatal(err)
 			}
 			fmt.Fprintf(conn, "bot%d\r\n", i)
-			for j := 0; j < 10; j++ {
-				time.Sleep(200 * time.Millisecond)
-				//log.Printf("Sending message #%d from bot #%d", j, i)
+			for j := 0; j < mesnum; j++ {
+				time.Sleep(100 * time.Millisecond)
 				fmt.Fprintf(conn, "Message #%d from bot #%d\n", j, i)
 			}
-			time.Sleep(200 * time.Millisecond)
+    		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			for j := 0; j < mesnum; j++ {
+				k := r.Intn(botnum)
+				time.Sleep(100 * time.Millisecond)
+				fmt.Fprintf(conn, "\\to:bot%d Message #%d from bot #%d to bot #%d\n", k, j, i, k)
+			}
+			time.Sleep(100 * time.Millisecond)
 			fmt.Fprintf(conn, "\\quit\r\n")
 			wg.Done()
 		}(i)
 	}
     wg.Wait()
-	time.Sleep(2000 * time.Millisecond)
-	//users.Lock()
-	//log.Println(count)
-	//users.Unlock()
+	time.Sleep(200 * time.Millisecond)
 }
